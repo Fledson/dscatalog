b42b69101bf4f6ee67c892887635f21cfebb1efb
Fledson Henrique <fledsonhenrique@gmail.com>
2022-09-15 21:45:03 -0300

    :sparkles: feat: DTO

--- backend/src/main/java/com/zihenx/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/zihenx/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.zihenx.dscatalog.resources;
 
-import com.zihenx.dscatalog.entities.Category;
+import com.zihenx.dscatalog.dto.CategoryDTO;
 import com.zihenx.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +19,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
 
         return ResponseEntity.ok().body(list);
     }
--- backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.zihenx.dscatalog.services;
 
+import com.zihenx.dscatalog.dto.CategoryDTO;
 import com.zihenx.dscatalog.entities.Category;
 import com.zihenx.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -14,9 +16,13 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return repository.findAll();
+    @Transactional
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+
+        List<CategoryDTO> listDto = list.stream().map( category -> new CategoryDTO(category) ).collect(Collectors.toList());
+
+        return listDto;
     }
 
 }
