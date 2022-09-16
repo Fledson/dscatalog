5fdbb22714798048bd387ec1ebda30ace842b6ea
Fledson Henrique <fledsonhenrique@gmail.com>
2022-09-15 22:17:57 -0300

    :sparkles: feat: adding feature to find category by id

--- backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,7 +17,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
 
@@ -25,4 +26,10 @@ public class CategoryService {
         return listDto;
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
+    }
 }
