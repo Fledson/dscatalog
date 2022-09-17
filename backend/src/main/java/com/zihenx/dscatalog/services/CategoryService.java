b4dd9de80699fac70fad5629b1c1925eb003af9d
Fledson Henrique <fledsonhenrique@gmail.com>
2022-09-16 21:10:33 -0300

    :sparkles: feat: adding exception handling

--- backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/zihenx/dscatalog/services/CategoryService.java
@@ -3,6 +3,8 @@ package com.zihenx.dscatalog.services;
 import com.zihenx.dscatalog.dto.CategoryDTO;
 import com.zihenx.dscatalog.entities.Category;
 import com.zihenx.dscatalog.repositories.CategoryRepository;
+import com.zihenx.dscatalog.services.exceptions.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +31,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.get();
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
 }
